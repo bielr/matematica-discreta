57e418d426c8751ea608c3076c2d0470be555b59
bielr <gabriel.riera@uib.es>
2022-04-26 20:58:36 +0200

    more typos

--- Entrega.java
+++ Entrega.java
@@ -21,7 +21,7 @@ import java.util.Set;
  *   molt.
  *
  * Per com està plantejada aquesta entrega, no necessitau (ni podeu) utilitzar cap `import`
- * addicional, ni mètodes de classes que no estiguin ja importades. El que sí podeu fer, es definir
+ * addicional, ni mètodes de classes que no estiguin ja importades. El que sí podeu fer és definir
  * tots els mètodes addicionals que volgueu (de manera ordenada i dins el tema que pertoqui).
  *
  * Podeu fer aquesta entrega en grups de com a màxim 3 persones, i necessitareu com a minim Java 8.
