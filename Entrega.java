573fd38676277fe8ec320c20680bf1377fe79bb9
Adria Alcala <adria.alcala@gmail.com>
2022-05-28 10:07:40 +0200

    Fix

--- Entrega.java
+++ Entrega.java
@@ -452,11 +452,11 @@ class Entrega {
       // `ordre i mida`
 
       assertThat(
-              Array.equals(exercici1(new int[][] { {0, 1, 0}, {1, 0, 1}, {0,1, 0}}), new int[] {3, 2})
+              Arrays.equals(exercici1(new int[][] { {0, 1, 0}, {1, 0, 1}, {0,1, 0}}), new int[] {3, 2})
       );
 
       assertThat(
-              Array.equals(exercici1(new int[][] { {0, 1, 0, 1}, {1, 0, 1, 1}, {0 , 1, 0, 1}, {1, 1, 1, 0}}), new int[] {4, 5})
+              Arrays.equals(exercici1(new int[][] { {0, 1, 0, 1}, {1, 0, 1, 1}, {0 , 1, 0, 1}, {1, 1, 1, 0}}), new int[] {4, 5})
       );
 
       // Exercici 2
